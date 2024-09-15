92614eb1ca51ac4e260e70310b1b96f6854e530c
Syed zainul abedin <szsimplesyed93@gmail.com>
2024-09-16 00:37:54 +0530

    Added Audio service

--- Assets/Scripts/AudioService.cs
+++ Assets/Scripts/AudioService.cs
@@ -1,9 +1,35 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 class AudioService : MonoBehaviour
 {
+    [SerializeField]
+    AudioSource audioSource;
+
+    [SerializeField]
+    private Sound[] sounds;
     private void Awake()
     {
-        
+        EventService.Instance.OnItemPurchased.AddListener(PlayPurchaseSound);        
     }
+
+    private void PlayPurchaseSound(Item item)
+    {
+        Sound sound = Array.Find(sounds, s => s.soundType == SoundType.Purchase);
+        audioSource.PlayOneShot(sound.clip);
+    }
+}
+
+[Serializable]
+struct Sound
+{
+    public SoundType soundType;
+    public AudioClip clip;
+}
+
+enum SoundType
+{
+    Purchase,
+    Gather
 }
